e4026a22253ee3306f70dd6b0fd144f5ad74f18f
mbozo <mbozo@dlb>
2018-11-02 17:40:32 -0300

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -62,4 +62,4 @@ self.addEventListener('fetch', e => {
     });
 
     e.respondWith(respuesta);
-})
+});
